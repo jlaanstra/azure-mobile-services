e10376fae840f8387e691aec1b01f687306dbe11
Johan Laanstra <jlaanstra1221@outlook.com>
2013-10-14 17:25:49 +0200

    Windows 8.1 demo app.

---
+++ extensions/offline/TodoItem.Win81/Behavior/BehaviorOfT.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml;
+
+namespace Microsoft.Xaml.Interactivity
+{
+    public class Behavior<T> : DependencyObject, IBehavior where T : DependencyObject
+    {
+        public virtual void Attach(DependencyObject associatedObject)
+        {
+            AssociatedObject = associatedObject as T;
+            OnAttached();
+        }
+
+        public virtual void Detach()
+        {
+            OnDetaching();
+        }
+
+        protected virtual void OnAttached()
+        {
+        }
+
+        protected virtual void OnDetaching()
+        {
+        }
+
+        public T AssociatedObject { get; private set; }
+
+        DependencyObject IBehavior.AssociatedObject
+        {
+            get { return this.AssociatedObject; }
+        }
+    }
+}
