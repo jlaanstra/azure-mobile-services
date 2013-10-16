078a2cbfaf541a09817b986aec77caa5b0e62f1e
Johan Laanstra <jlaanstra1221@outlook.com>
2013-10-16 16:20:40 +0200

    Add new SQLite WinRT wrapper.

---
+++ extensions/offline/sqlwinrt_49f8079f900d/SQLiteWinRT/Update_Project_SQLite_SDK_References.ps1
@@ -0,0 +1,11 @@
+﻿# Brian Wilhite
+# bcwilhite@live.com
+# TechEd 2013 NA June 5th, 2013
+
+$FolderPath = "C:\Program Files (x86)\Microsoft SDKs\Windows\v8.0\ExtensionSDKs\SQLite.WinRT"
+$FilePath   = ".\SQLiteWinRT.vcxproj"
+$VersionNumber = Get-ChildItem -Path $FolderPath | Where-Object {$_.Extension -ne ".deleteme"} | Select-Object -ExpandProperty Name
+$FileContents = Get-Content -Path $FilePath
+$FileContents = $FileContents | ForEach-Object {$_ -replace '<SDKReference Include="SQLite.WinRT, Version=(\d.?)+', "<SDKReference Include=`"SQLite.WinRT, Version=$VersionNumber"}
+$FileContents = $FileContents | ForEach-Object {$_ -replace '<Import Project="\$\(MSBuildProgramFiles32\)\\Microsoft SDKs\\Windows\\v8.0\\ExtensionSDKs\\SQLite.WinRT\\(\d.?)+', "<Import Project=`"`$(MSBuildProgramFiles32)\Microsoft SDKs\Windows\v8.0\ExtensionSDKs\SQLite.WinRT\$VersionNumber"}
+$FileContents | Out-File $FilePath -Encoding ascii -Force
