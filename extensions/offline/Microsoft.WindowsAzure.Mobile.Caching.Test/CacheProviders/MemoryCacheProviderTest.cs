aea931c8ad9271b11cb270f53b610dec994fd4cd
Johan Laanstra <jlaanstra1221@outlook.com>
2013-11-13 14:50:59 +0100

    Added Test for two providers.
    Fix bug in MemoryCacheProvider.
    Fix bug where CacheHandler wasn't checking if the request should be
    cached.

---
+++ extensions/offline/Microsoft.WindowsAzure.Mobile.Caching.Test/CacheProviders/MemoryCacheProviderTest.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Microsoft.WindowsAzure.MobileServices.Caching.Test
+{
+    [TestClass]
+    public class MemoryCacheProviderTest
+    {
+        private Uri testUri = new Uri("http://localhost/");
+        private StringContent testContent = new StringContent("Test", Encoding.UTF8);
+
+        [TestMethod]
+        public void IsByDefaultChachingEverything()
+        {
+            var memory = new MemoryCacheProvider(TimeSpan.FromMinutes(1));
+
+            Assert.IsTrue(memory.ProvidesCacheForRequest(new Uri("http://localhost/")));
+        }
+
+        [TestMethod]
+        public async Task ReadShouldCacheForTheTimeSpanProvided()
+        {
+            var memory = new MemoryCacheProvider(TimeSpan.FromSeconds(5));
+            HttpContent empty = new StringContent(string.Empty);
+
+            HttpContent resultContent = testContent;
+
+            Func<Uri, HttpContent, HttpMethod, Task<HttpContent>> getResponse = (u, c, m) =>
+            {
+                return Task.FromResult(resultContent);
+            };
+
+            HttpContent result = await memory.Read(testUri, getResponse);
+            Assert.AreEqual(testContent, result);
+
+            resultContent = empty;
+
+            result = await memory.Read(testUri, getResponse);
+            Assert.AreEqual(testContent, result);
+
+            await Task.Delay(5000);
+
+            result = await memory.Read(testUri, getResponse);
+            Assert.AreEqual(empty, result);
+        }
+    }
+}
--- extensions/offline/Microsoft.WindowsAzure.Mobile.Caching/CacheProviders/BaseCacheProvider.cs
+++ extensions/offline/Microsoft.WindowsAzure.Mobile.Caching/CacheProviders/BaseCacheProvider.cs
@@ -9,24 +9,24 @@ namespace Microsoft.WindowsAzure.MobileServices.Caching
 {
     public abstract class BaseCacheProvider : ICacheProvider
     {
-        public virtual Task<HttpContent> Read(Uri requestUri, OptionalFunc<Uri, HttpContent, HttpMethod, Task<HttpContent>> getResponse)
+        public virtual Task<HttpContent> Read(Uri requestUri, Func<Uri, HttpContent, HttpMethod, Task<HttpContent>> getResponse)
         {
-            return getResponse(requestUri);
+            return getResponse(requestUri, null, HttpMethod.Get);
         }
 
-        public virtual Task<HttpContent> Insert(Uri requestUri, HttpContent content, OptionalFunc<Uri, HttpContent, HttpMethod, Task<HttpContent>> getResponse)
+        public virtual Task<HttpContent> Insert(Uri requestUri, HttpContent content, Func<Uri, HttpContent, HttpMethod, Task<HttpContent>> getResponse)
         {
-            return getResponse(requestUri, content);
+            return getResponse(requestUri, content, HttpMethod.Post);
         }
 
-        public virtual Task<HttpContent> Update(Uri requestUri, HttpContent content, OptionalFunc<Uri, HttpContent, HttpMethod, Task<HttpContent>> getResponse)
+        public virtual Task<HttpContent> Update(Uri requestUri, HttpContent content, Func<Uri, HttpContent, HttpMethod, Task<HttpContent>> getResponse)
         {
-            return getResponse(requestUri, content);
+            return getResponse(requestUri, content, new HttpMethod("PATCH"));
         }
 
-        public virtual Task<HttpContent> Delete(Uri requestUri, OptionalFunc<Uri, HttpContent, HttpMethod, Task<HttpContent>> getResponse)
+        public virtual Task<HttpContent> Delete(Uri requestUri, Func<Uri, HttpContent, HttpMethod, Task<HttpContent>> getResponse)
         {
-            return getResponse(requestUri);
+            return getResponse(requestUri, null, HttpMethod.Delete);
         }
 
         public virtual bool ProvidesCacheForRequest(Uri requestUri)
