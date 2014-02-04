676548911deb1597adbeb9d60a970650465d720b
Johan Laanstra <jlaanstra1221@outlook.com>
2014-02-04 13:43:01 +0100

    .Net45 library.

---
+++ extensions/offline/Microsoft.WindowsAzure.Mobile.Caching.Net45/NetworkInformation.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Microsoft.WindowsAzure.MobileServices
+{
+    public class NetworkInformation : INetworkInformation
+    {
+        public Task<bool> IsConnectedToInternet()
+        {
+            return Task.Run(() =>
+            {
+                int Desc;
+                return InternetGetConnectedState(out Desc, 0);
+            });
+        }
+
+        //Creating the extern function...
+        [DllImport("wininet.dll")]
+        private extern static bool InternetGetConnectedState(out int Description, int ReservedValue);
+    }
+}
