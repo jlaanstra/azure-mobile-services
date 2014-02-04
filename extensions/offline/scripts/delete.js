7d216b754481647ec2d9c13f4e602304af225a59
Johan Laanstra <jlaanstra1221@outlook.com>
2014-02-04 13:30:33 +0100

    Small script changes.

--- extensions/offline/scripts/delete.js
+++ extensions/offline/scripts/delete.js
@@ -1,10 +1,18 @@
 /// <reference path="mobileservices.intellisense.js" />
 
 function del(id, user, request) {
+
+    var version = request.parameters.version;
+    //existing items must have a timestamp
+    if (version === undefined) {
+        request.respond(statusCodes.BAD_REQUEST,
+        "delete operation must specify the version parameter");
+        return;
+    }
     
     var tableName = tables.current.getTableName();
     
-    //only delete if false to safe an unnecessary changes
+    //only delete if false to safe an unnecessary change
     var sqlUpdate = "UPDATE " + tableName + " SET isDeleted = 'True' WHERE id = ? AND isDeleted = 'False'";
         
     mssql.query(sqlUpdate, {
