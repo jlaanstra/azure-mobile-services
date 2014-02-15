763208167dd30402bb03f5916b2f8095c4a545a1
Johan Laanstra <jlaanstra1221@outlook.com>
2014-02-16 00:30:00 +0100

    Add test from storage.

---
+++ extensions/offline/Microsoft.WindowsAzure.Mobile.Caching.Test/Storage/SQLiteStructuredStorageTest.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Newtonsoft.Json.Linq;
+
+namespace Microsoft.WindowsAzure.MobileServices.Caching.Test.Storage
+{
+    [TestClass]
+    public class SQLiteStructuredStorageTest
+    {
+        private SQLiteStructuredStorage storage;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            storage = new SQLiteStructuredStorage("test");
+        }
+
+        [TestMethod]
+        public void GetColumnsFromItemsTest()
+        {
+            JArray array = new JArray();
+            array.Add(new JObject()
+            {
+                { "integer", 1 },
+                { "value1", "string"},
+                { "boolean", true }
+            });
+
+            IDictionary<string, Column> cols = storage.GetColumnsFromItems(array);
+
+            Assert.AreEqual(storage.defaultColumns.Count + 3, cols.Count);
+
+            Assert.IsTrue(cols.ContainsKey("integer"));
+            // json.net makes int long
+            Assert.AreEqual(typeof(long), cols["integer"].GetClrDataType());
+            Assert.IsTrue(cols.ContainsKey("value1"));
+            Assert.AreEqual(typeof(string), cols["value1"].GetClrDataType());
+            Assert.IsTrue(cols.ContainsKey("boolean"));
+            Assert.AreEqual(typeof(bool), cols["boolean"].GetClrDataType());
+        }
+
+        [TestMethod]
+        public void GetColumnsFromItemsFallthroughNullTest()
+        {
+            JArray array = new JArray();
+            array.Add(new JObject()
+            {
+                { "integer", 1 },
+                { "value1", "string"},
+                { "boolean", true },
+                { "null", null }
+
+            });
+            array.Add(new JObject()
+            {
+                { "integer", 1 },
+                { "value1", "string"},
+                { "boolean", true },
+                { "null", DateTime.Now }
+            });
+
+            IDictionary<string, Column> cols = storage.GetColumnsFromItems(array);
+
+            Assert.AreEqual(storage.defaultColumns.Count + 4, cols.Count);
+
+            Assert.IsTrue(cols.ContainsKey("integer"));
+            // json.net makes int long
+            Assert.AreEqual(typeof(long), cols["integer"].GetClrDataType());
+            Assert.IsTrue(cols.ContainsKey("value1"));
+            Assert.AreEqual(typeof(string), cols["value1"].GetClrDataType());
+            Assert.IsTrue(cols.ContainsKey("boolean"));
+            Assert.AreEqual(typeof(bool), cols["boolean"].GetClrDataType());
+            Assert.IsTrue(cols.ContainsKey("null"));
+            Assert.AreEqual(typeof(DateTime), cols["null"].GetClrDataType());
+        }
+    }
+}
