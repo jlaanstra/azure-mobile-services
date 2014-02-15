085dbe17b3cb9e6d7e1beac4c21b723b0ba65867
Johan Laanstra <jlaanstra1221@outlook.com>
2014-02-16 00:24:53 +0100

    Fix test project.

--- extensions/offline/Microsoft.WindowsAzure.Mobile.Caching.Test/Properties/AssemblyInfo.cs
+++ extensions/offline/Microsoft.WindowsAzure.Mobile.Caching.Test/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Microsoft.WindowsAzure.Mobile.Caching.Test")]
-[assembly: AssemblyCopyright("Copyright ©  2013")]
+[assembly: AssemblyCopyright("Copyright ©  2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("6f73023e-76f4-4812-9155-ec5451f5d54b")]
+[assembly: Guid("d4bd5c3c-8f26-4eb6-9c2a-782b3dc61897")]
 
 // Version information for an assembly consists of the following four values:
 //
