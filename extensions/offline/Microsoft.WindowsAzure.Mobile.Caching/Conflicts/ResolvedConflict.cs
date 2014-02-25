9bb0d1b4e28514dbfcb9f01ca3b553f8ab180e04
Johan Laanstra <jlaanstra1221@outlook.com>
2014-02-25 20:52:59 +0100

    Additional conflict classes.

---
+++ extensions/offline/Microsoft.WindowsAzure.Mobile.Caching/Conflicts/ResolvedConflict.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json.Linq;
+
+namespace Microsoft.WindowsAzure.MobileServices.Caching
+{
+    public class ResolvedConflict
+    {
+        internal ResolvedConflict(string resolveStrategy, ConflictType type, IEnumerable<JObject> results)
+        {
+            this.ResolveStrategy = resolveStrategy;
+            this.Type = type;
+            this.Results = results;
+        }
+
+        public string ResolveStrategy { get; private set; }
+
+        public ConflictType Type { get; private set; }
+
+        public IEnumerable<JObject> Results { get; private set; }
+    }
+}
---
+++ extensions/offline/Microsoft.WindowsAzure.Mobile.Caching/Exceptions/MobileServiceConflictsResolvedException.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Microsoft.WindowsAzure.MobileServices.Caching
+{
+    public class MobileServiceConflictsResolvedException : Exception
+    {
+        public MobileServiceConflictsResolvedException(IList<ResolvedConflict> conflicts)
+            : base("One or more conflicts where resolved while synchronizing.")
+        {
+            this.Conflicts = conflicts;
+        }
+
+        public IEnumerable<ResolvedConflict> Conflicts { get; private set; }
+    }
+}
