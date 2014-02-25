80dfc4ebb5db41c5abedd65e43d2c3506cec39ce
Johan Laanstra <jlaanstra1221@outlook.com>
2014-02-26 00:17:24 +0100

    Add extension method to easily enabled offline.

---
+++ extensions/offline/Microsoft.WindowsAzure.Mobile.Caching/Extensions/MobileServiceClientExtensions.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Microsoft.WindowsAzure.MobileServices.Caching
+{
+    public static class MobileServiceClientExtensions
+    {
+        public static MobileServiceClient EnableOfflineDataCapabilitiesForTables(this MobileServiceClient This, INetworkInformation networkInfo, IConflictResolver localConflictResolver = null)
+        {
+            IStructuredStorage storage = new SQLiteStructuredStorage("Microsoft.WindowsAzure.Mobile.Caching.db");
+            ISynchronizer synchronizer = new TimestampSynchronizer(storage, localConflictResolver ?? new NoConflictResolver());
+            ICacheProvider cacheProvider = new TimestampCacheProvider(storage, networkInfo, synchronizer, u => u.OriginalString.Contains("/tables/"));
+
+            return new MobileServiceClient(This.ApplicationUri, This.ApplicationKey, new CacheHandler(cacheProvider), new HttpClientHandler());
+        }
+    }
+}
